b71f96ced08051a04afc8e37845a0f1120a5d5b9
Wanting Zeng <wtzeng@ucdavis.edu>
2022-04-20 09:41:51 -0700

    feat: basic prototype for music classifier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AudioFileIcon from "@mui/icons-material/AudioFile";
+import SendIcon from "@mui/icons-material/Send";
+import Typography from "@mui/material/Typography";
+import placeholder from "./assets/adrian-regeci-SAS0lq2QGLs-unsplash.jpg";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Stack direction="row" backgroundColor="#222831" height="100vh">
+        <Stack
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
+          backgroundColor="#393E46"
+          width="60%"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h5" color="white">
+            Music Classifier
+          </Typography>
+          <label htmlFor="contained-button-file">
+            <Input id="contained-button-file" type="file" />
+            <Button
+              variant="contained"
+              component="span"
+              startIcon={<AudioFileIcon />}
+            >
+              Upload
+            </Button>
+          </label>
+          <Button variant="contained" endIcon={<SendIcon />}>
+            Classify
+          </Button>
+        </Stack>
+        <img src={placeholder} alt="music" />
+      </Stack>
+    </>
   );
 }
 
